ded00f3a0444d26e0c32f8e44588cc8e23c2e3f9
Elbadil <elbaliadil@gmail.com>
2024-02-17 15:01:31 +0100

    Route Arguments

--- base/views.py
+++ base/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-
+rooms = [
+    {'id': 1, 'name': 'Lets learn Python!'},
+    {'id': 2, 'name': 'Lets learn C++!'},
+    {'id': 3, 'name': 'Lets learn JavaScript!'},
+]
 
 def home(request):
     """"""
-    return render(request, 'home.html')
+    return render(request, 'base/home.html', {'rooms': rooms})
 
-def room(request):
+def room(request, id):
     """"""
-    return render(request, 'room.html')
+    s_room = None
+    for room in rooms:
+        if str(room['id']) == id:
+            s_room = room
+    return render(request, 'base/room.html', {'room': s_room})
